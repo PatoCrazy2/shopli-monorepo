f852c9163b05d02bce233c2d52798344873e8787
Emilio Escobedo <emiliosalu4@gmail.com>
2026-05-05 15:52:43 -0600

    fix: explicit output directory for POS

--- apps/pos/vite.config.ts
+++ apps/pos/vite.config.ts
@@ -6,6 +6,10 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   plugins: [
     react(),
     tailwindcss(),
